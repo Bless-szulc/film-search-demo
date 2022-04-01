0e8e3631a152493386a0f95390e1fccda50f27f1
Bless-szulc <bless.szulc@gmail.com>
2022-04-02 01:33:54 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import response from './apifetch.js';
+import React, {useEffect, useState} from "react";
+
+const MovieImage = ({link}) => {
+    return <img src={link}/>
+}
 
 function App() {
+    const [jsonData, setJsonData] = useState();
+    const api_key = "d1b0cd372f3245a37cbdde3cb4caa572";
+    const imagepage = 'https://image.tmdb.org/t/p/w500';
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/550?api_key=${api_key}`)
+            .then(response => response.json())
+            .then(json => {
+                console.log(json);
+                setJsonData(json);
+            })
+    },[])
+
+    console.log(jsonData);
+    console.log(`${imagepage}${jsonData?.poster_path}`);
+    console.log(`${imagepage}${jsonData?.backdrop_path}`);
+
+    const img1 = `${imagepage}${jsonData?.poster_path}`;
+    const img2 = `${imagepage}${jsonData?.backdrop_path}`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <MovieImage link={img1}/>
+        <MovieImage link={img2}/>
     </div>
   );
 }
---
+++ src/apifetch.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// const api_key = "d1b0cd372f3245a37cbdde3cb4caa572"
+//
+// function apicall() {
+//     const response = fetch(`https://api.themoviedb.org/3/movie/550?api_key=${api_key}`);
+//     console.log(response);
+//     return response.json()
+// }
+//
+// const response = apicall()
+// export default response;
